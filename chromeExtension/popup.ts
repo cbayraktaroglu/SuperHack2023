575b2101ea805598056cafd07685b500e22820ed
Ege Arıkan <arikanege@gmail.com>
2023-08-13 14:03:47 +0200

    Updated popup.ts

--- chromeExtension/popup.ts
+++ chromeExtension/popup.ts
@@ -52,24 +52,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
         const fileName = jsonResponse.Data.file_name;
         const fileExtension = fileName.substring(fileName.lastIndexOf('.'));
-        const status = jsonResponse.Status;
 
         // Extract file name and type from the uploaded file
         const fileType = mimeTypes[fileExtension];
 
-        // Create a new Uint8Array from the buffer
-        //const uint8Array = new Uint8Array(buffer);
-        //console.log(uint8Array);
-
-        //const blob = new Blob([buffer], { type: 'image/gif' });
         const blob = new Blob([returnBufferBytes], { type: fileType });
         const blobUrl = URL.createObjectURL(blob);
 
-
-        console.log(blob);
-
-        // Display the preview image
-        //await sleep(100);
 
         chrome.tabs.create({ url: blobUrl });
 
@@ -201,43 +190,3 @@ function sleep(ms: number): Promise<void> {
   });
 }
 
-/*
-document.addEventListener('DOMContentLoaded', function() {
-    const fileInput = document.getElementById('fileInput') as HTMLInputElement;
-    const uploadButton = document.getElementById('uploadButton') as HTMLButtonElement;
-  
-    uploadButton.addEventListener('click', async () => {
-      const file = fileInput.files?.[0];
-  
-      if (file) {
-        const reader = new FileReader();
-        reader.onload = async (event) => {
-          const fileContents = event.target?.result as string;
-          
-          try {
-            const response = await fetch('http://localhost:3000/decodePrivate', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json' // Set the Content-Type to JSON
-              },
-              body: fileContents // Send the JSON data as the request body
-            });
-  
-            if (response.ok) {
-              const buffer = await response.arrayBuffer();
-              const blob = new Blob([buffer]);
-              const url = URL.createObjectURL(blob);
-  
-              chrome.tabs.create({ url });
-            } else {
-              console.error('API request failed:', response.status, response.statusText);
-            }
-          } catch (error) {
-            console.error('Error:', error);
-          }
-        };
-        reader.readAsText(file); // Read the file contents as text
-      }
-    });
-  });
- */ 
